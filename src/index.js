aa9f9d2dac98cc8aed375bd59daad3327e952fd2
F7MH2R <skyicef7@gmail.com>
2023-09-25 23:57:28 -0600

    jsx de musto 1 de login

--- src/index.js
+++ src/index.js
@@ -2,14 +2,16 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 //import App from "./App";
+//import Login from "./componentes/pages/login";
+import AYUDA from "./componentes/pages/Ayuda";
 import reportWebVitals from "./reportWebVitals";
 import { Route, BrowserRouter as Router } from "react-router-dom";
-import LOGIN from "./componentes/pages/Login";
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <Router>
-      <LOGIN />
+      <Login />
     </Router>
   </React.StrictMode>
 );
