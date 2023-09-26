b83c29d0b09a529170baaf5853a30a3e72de9467
F7MH2R <skyicef7@gmail.com>
2023-09-25 23:46:11 -0600

    index con LOGIN

--- src/index.js
+++ src/index.js
@@ -2,10 +2,9 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 //import App from "./App";
-import LOGIN from "./componentes/pages/Login";
 import reportWebVitals from "./reportWebVitals";
 import { Route, BrowserRouter as Router } from "react-router-dom";
-
+import LOGIN from "./componentes/pages/Login";
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
