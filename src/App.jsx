331b70535e0fce5543d720cfeee23f9950d2e391
xaldakr <xavieravilaposada@gmail.com>
2023-09-26 12:17:29 -0600

    agregar

--- src/App.jsx
+++ src/App.jsx
@@ -4,11 +4,12 @@ import { Explorar } from "./componentes/pages/explorar";
 import Login from "./componentes/pages/login";
 import Signup from "./componentes/pages/singup";
 import Navbarra from "./componentes/secciones/Navbarra";
+import Home from "./componentes/home/Home";
 function App() {
   return (
     <div className="App">
       <Navbarra data={true} />
-      <Signup />
+      <Home />
       <Routes></Routes>
     </div>
   );
