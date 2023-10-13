847f2e6173118bc9af0ce3ca94321e0b5a06f40c
F7MH2R <skyicef7@gmail.com>
2023-10-12 22:47:33 -0600

    h77

--- src/componentes/pages/quienessomos.jsx
+++ src/componentes/pages/quienessomos.jsx
@@ -1,35 +1,38 @@
 import React from "react";
 import bg from "../home/media/bgQuienes.mp4";
+import "../../App.css";
 
 function QuienesSomos() {
   return (
     <div className="quienes-somos-page">
-         <video autoPlay loop muted className="video-bg">
+      <video autoPlay loop muted className="video-bg">
         <source src={bg} type="video/mp4" />
         Tu navegador no soporta la reproducción de videos.
       </video>
       <div className="quienes-somos-overlay"></div>
       <div className="quienes-somos-content">
-        <h77 style={{ textAlign: "left" }}>QUIENES SOMOS</h77>
+        <h2 className="h77" style={{ textAlign: "left" }}>
+          QUIENES SOMOS
+        </h2>
         <div className="parrafos-container">
           <div className="parrafoq">
             <p>
-            Lorem Ipsum es simplemente el texto de relleno 
-            de las imprentas y archivos de texto. Lorem Ipsum ha 
-             sido el texto de relleno estándar de las industrias 
-             desde el año 1500, cuando un impresor (N. del T. person
-             a que se dedica a la imprenta) desconocido usó una galería
-              de textos y los mezcló de tal manera que logró hacer un libro
-               de textos especimen.
+              Lorem Ipsum es simplemente el texto de relleno de las imprentas y
+              archivos de texto. Lorem Ipsum ha sido el texto de relleno
+              estándar de las industrias desde el año 1500, cuando un impresor
+              (N. del T. person a que se dedica a la imprenta) desconocido usó
+              una galería de textos y los mezcló de tal manera que logró hacer
+              un libro de textos especimen.
             </p>
           </div>
           <div className="parrafoq">
             <p>
-            Lorem Ipsum es simplemente el texto de relleno de las imprentas
-             y archivos de texto. Lorem Ipsum ha sido el texto de relleno estándar
-              de las industrias desde el año 1500, cuando un impresor (N. del T.
-               persona que se dedica a la imprenta) desconocido usó una galería de
-                textos y los mezcló de tal manera que logró hacer un libro de textos especimen.
+              Lorem Ipsum es simplemente el texto de relleno de las imprentas y
+              archivos de texto. Lorem Ipsum ha sido el texto de relleno
+              estándar de las industrias desde el año 1500, cuando un impresor
+              (N. del T. persona que se dedica a la imprenta) desconocido usó
+              una galería de textos y los mezcló de tal manera que logró hacer
+              un libro de textos especimen.
             </p>
           </div>
         </div>
