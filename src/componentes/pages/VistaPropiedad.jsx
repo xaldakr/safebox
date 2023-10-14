be26c3528cd8c2d6992234c05d232f344d39c328
Your Name <you@example.com>
2023-10-14 15:22:01 -0600

    commit?

--- src/componentes/pages/VistaPropiedad.jsx
+++ src/componentes/pages/VistaPropiedad.jsx
@@ -10,6 +10,7 @@ import {
 } from "react-bootstrap";
 import img1 from "../home/media/casa1.jpg";
 import img2 from "../home/media/casa2.jpg";
+import { Link } from "react-router-dom";
 
 const bg_Propiedad = {
   color: "#CFC0BD",
@@ -19,6 +20,9 @@ const styledButton = {
   width: "100%",
   backgroundColor: "#CFC0BD",
   color: "#2A605A",
+  textDecoration: "none",
+  textAlign: "center",
+  padding: "0.4rem",
   border: "3px solid #2A605A",
 };
 
@@ -30,7 +34,9 @@ export const VistaPropiedad = () => {
       <Container>
         <Row>
           <Col xs={12} md={6}>
-            <h1 className="text-center">Titulo de la propiedad</h1>{" "}
+            <h1 className="text-center" style={{ color: "black" }}>
+              Titulo de la propiedad
+            </h1>{" "}
             <h2
               style={{
                 color: "#141223",
@@ -65,7 +71,7 @@ export const VistaPropiedad = () => {
               </Carousel.Item>
             </Carousel>
             <div className="mt-5">
-              <h3>Descripción:</h3>
+              <h3 style={{ color: "black" }}>Descripción:</h3>
               <p>
                 Lorem ipsum dolor sit amet consectetur adipisicing elit. Illo
                 deleniti exercitationem, ratione recusandae, libero fugit
@@ -74,7 +80,7 @@ export const VistaPropiedad = () => {
               </p>
             </div>
             <Row>
-              <h2>Datos</h2>
+              <h2 style={{ color: "black" }}>Datos</h2>
               <Col xs={6} md={6} className="mb-3">
                 <div
                   style={{
@@ -82,7 +88,7 @@ export const VistaPropiedad = () => {
                   }}
                 >
                   {" "}
-                  <h3>Habitaciones:</h3>
+                  <h3 style={{ color: "white" }}>Habitaciones:</h3>
                   <h3>0</h3>
                 </div>
               </Col>
@@ -93,7 +99,7 @@ export const VistaPropiedad = () => {
                   }}
                 >
                   {" "}
-                  <h3>Baños:</h3>
+                  <h3 style={{ color: "white" }}>Baños:</h3>
                   <h3>0</h3>
                 </div>
               </Col>
@@ -104,7 +110,7 @@ export const VistaPropiedad = () => {
                   }}
                 >
                   {" "}
-                  <h3>Patios:</h3>
+                  <h3 style={{ color: "white" }}>Patios:</h3>
                   <h3>0</h3>
                 </div>
               </Col>
@@ -115,7 +121,7 @@ export const VistaPropiedad = () => {
                   }}
                 >
                   {" "}
-                  <h3>Garajes:</h3>
+                  <h3 style={{ color: "white" }}>Garajes:</h3>
                   <h3>0</h3>
                 </div>
               </Col>
@@ -127,7 +133,7 @@ export const VistaPropiedad = () => {
                 }}
               >
                 {" "}
-                <h3>Extensión:</h3>
+                <h3 style={{ color: "white" }}>Extensión:</h3>
                 <h3>0x0</h3>
               </div>
             </Col>
@@ -138,7 +144,7 @@ export const VistaPropiedad = () => {
                 }}
               >
                 {" "}
-                <h3>Departamento:</h3>
+                <h3 style={{ color: "white" }}>Departamento:</h3>
                 <h3> Santa Ana</h3>
               </div>
             </Col>
@@ -149,77 +155,34 @@ export const VistaPropiedad = () => {
                 }}
               >
                 {" "}
-                <h3>Municipio:</h3>
+                <h3 style={{ color: "white" }}>Municipio:</h3>
                 <h3>Chalchuapa</h3>
-              </div>
-            </Col>
-            <Col xs={12} md={12} className="mb-3">
-              <div
-                style={{
-                  display: "flex",
-                }}
-              >
-                {" "}
-                <h3>Punto de referencia:</h3>
               </div>
             </Col>
           </Col>
           <Col xs={12} md={6}>
-            <h2 className="text-center">Agendar</h2>
-            <Form
-              style={{
-                border: "3px solid black",
-                borderRadius: "10px",
-              }}
+            <h2 className="text-center" style={{ color: "black" }}>
+              Datos de contacto
+            </h2>
+            <section
+              className="mt-5"
+              style={{ display: "flex", flexDirection: "column", gap: "10px" }}
             >
-              <FloatingLabel
-                controlId="floatingInput"
-                label="Tu nombre"
-                className="mb-3 mt-3"
-                style={{ width: "90%", margin: "0% 5%" }}
-              >
-                <Form.Control type="text" placeholder="nombre" />
-              </FloatingLabel>
-              <FloatingLabel
-                controlId="floatingInput"
-                label="Tu Correo"
-                className="mb-3 mt-3"
-                style={{ width: "90%", margin: "0% 5%" }}
-              >
-                <Form.Control type="email" placeholder="correo@correo.com" />
-              </FloatingLabel>
-              <FloatingLabel
-                controlId="floatingInput"
-                label="Tu teléfono"
-                className="mb-3 mt-3"
-                style={{ width: "90%", margin: "0% 5%" }}
-              >
-                <Form.Control type="text" placeholder="0000-0000" />
-              </FloatingLabel>
-              <FloatingLabel controlId="floatingTextarea2" label="Mensaje">
-                <Form.Control
-                  as="textarea"
-                  placeholder="Deja un comentario aqui"
-                  style={{ height: "100px" }}
-                />
-              </FloatingLabel>
-              <Button style={styledButton} className="mt-5 mb-5">
+              <h4>Nombre del vendedor: Bryan Vásquez</h4>
+              <h4>correo del vendedor: correo@correo.com</h4>
+              <h4>Teléfono del vendedor: 0000-0000</h4>
+              <Link to={"agendar"} style={styledButton} className="mt-5 mb-5">
                 Contactar
-              </Button>
-            </Form>
+              </Link>
+            </section>
             <iframe
-              title="Ubicación en el Mapa"
-              src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3871.596340267907!2d-89.55097142569616!3d13.982616191909331!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x8f62e6202f8eeca1%3A0x976b0fd1a95a4d8a!2sUniversidad%20Cat%C3%B3lica%20de%20El%20Salvador!5e0!3m2!1ses-419!2ssv!4v1693027294047!5m2!1ses-419!2ssv"
               width="100%"
-              height="450"
-              style={{
-                border: "2px solid #CFC0BD ",
-                borderRadius: "10px ",
-                marginTop: "5%",
-              }}
-              allowFullScreen=""
-              loading="lazy"
-              referrerPolicy="no-referrer-when-downgrade"
+              height="415"
+              src="https://www.youtube.com/embed/iVCy2Frm21I?si=VJsnXLpz5a4QCMxd"
+              title="YouTube video player"
+              frameborder="0"
+              allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+              allowfullscreen
             ></iframe>
           </Col>
         </Row>
@@ -227,7 +190,9 @@ export const VistaPropiedad = () => {
           <Col>
             <div>
               {" "}
-              <h3>Ubicación exacta:</h3>
+              <h3 style={{ color: "black", textAlign: "center" }}>
+                Ubicación exacta:
+              </h3>
               <p>
                 {" "}
                 Lorem ipsum dolor sit amet consectetur adipisicing elit. Eum
